8ddf4233c7904c45ded706f9d30533ee1d087928
shimo91 <shimonaprakash@gmail.com>
2023-12-22 14:52:48 +0400

    added common button and common input

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import StyledButton from './ui/CommonButton'
+import InputWithIcon from './ui/CommonInput';
+import { ReactComponent as SmsIcon } from './assets/icons/sms.svg'; 
+import { ReactComponent as LockIcon } from './assets/icons/lock.svg'; 
+import { ReactComponent as EyeIcon } from './assets/icons/eye-slash.svg'; 
 
 export default function App() {
+  const bgcolor='var(--Primary, linear-gradient(100deg, #ED5DCD -2.24%, rgba(95, 93, 215, 0.71) 98.06%))';
+  const bgcolor2='var(--Selection, #4A4458);';
+
   return (
     <div>
       Hi
+      <br/>
+      <StyledButton background={bgcolor}>Sign in</StyledButton>
+      <br/>
+      <StyledButton background={bgcolor2} border="2px solid var(--White-20, rgba(255, 255, 255, 0.20));">Sign in</StyledButton>
+      <br/>
+      <InputWithIcon
+        icon={<SmsIcon  />} 
+        placeholder="Enter your email"
+      />
+      <br/>
+      <InputWithIcon
+        icon={<LockIcon  />} 
+        placeholder="Enter your password"
+        iconright={<EyeIcon  />} 
+      />
     </div>
   )
 }
---
+++ src/ui/CommonInput.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import styled, { css } from 'styled-components';
+
+const InputContainer = styled.div`
+  position: relative;
+  display: flex;
+  width: 482px;
+  height: 56px;
+  padding: 16px;
+  align-items: center;
+  gap: 8px;
+  border: 1px solid rgba(255, 255, 255, 0.20);
+  background: var(--inner, #39383D);
+  border-radius: 4px;
+`;
+
+const StyledInput = styled.input`
+  
+color: var(--Color-Dark-Dark-2, #87898E);
+font-feature-settings: 'clig' off, 'liga' off;
+/* Small Caption/ Medium */
+font-family: Inter;
+font-size: 14px;
+font-style: normal;
+font-weight: 400;
+line-height: 16px; /* 114.286% */
+flex: 1 0 0;
+background: var(--inner, #39383D);
+border:none;
+&:focus {
+    outline: none; // Disable the default focus outline
+    border: none;  // Set border to none when focusing
+    // Add any additional styles you want when the input is focused
+  }
+`;
+
+const IconContainer = styled.div`
+display: flex;
+width: 24px;
+height: 24px;
+justify-content: center;
+align-items: center;
+flex-shrink: 0;
+color:#87898E;
+`;
+
+const InputWithIcon = ({ icon, placeholder,iconright, ...props }) => {
+  return (
+    <InputContainer>
+      {icon && <IconContainer>{icon}</IconContainer>}
+      <StyledInput placeholder={placeholder} {...props} />
+      {iconright && <IconContainer>{iconright}</IconContainer>}
+    </InputContainer>
+  );
+};
+
+export default InputWithIcon;
