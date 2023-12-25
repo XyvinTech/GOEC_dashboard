12cdb4000def1b7ff5427b74e900b9f783f5bf61
XyvinTech <141544499+XyvinTech@users.noreply.github.com>
2023-12-25 16:56:09 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
 import React from "react";
-import StyledTable from "./ui/styledTable";
-import { DummyData } from "./assets/json/TableData";
-import "./App.css";
-
+import RouteRenderer from "./core/routes/route-renderer";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./theme/index";
-import DashboardLayout from "./layout/dashboardLayout";
 
 export default function App() {
-
-  const headers = [
-    "OCCP Txn ID",
-    "User Name",
-    "Charge Station Name" ,
-    "status"
-  ];
-
-
-
   return (
-    <ThemeProvider theme={theme}>
-      <DashboardLayout>
-        <StyledTable headers={headers} data={DummyData} />
-      </DashboardLayout>
-    </ThemeProvider>
-
+    <div>
+      <ThemeProvider theme={theme}>
+        <RouteRenderer />
+      </ThemeProvider>
+    </div>
   );
 }
