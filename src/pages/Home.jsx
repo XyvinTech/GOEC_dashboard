5d91e4856b30d1b452c05f04f8b39e9e7c8201be
XyvinTech <141544499+XyvinTech@users.noreply.github.com>
2023-12-25 17:01:45 +0530

    routing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+import StyledTable from "../ui/styledTable.jsx";
+import { DummyData } from "../assets/json/TableData";
+
+import DashboardLayout from '../layout/dashboardLayout.jsx'
 
 export default function Home() {
+
+  const headers = [
+    "OCCP Txn ID",
+    "User Name",
+    "Charge Station Name" ,
+    "status"
+  ];
+
+
+
   return (
-    <div>Home</div>
-  )
+      <DashboardLayout>
+        <StyledTable headers={headers} data={DummyData} />
+      </DashboardLayout>
+
+  );
 }
