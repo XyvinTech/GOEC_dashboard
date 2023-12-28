6ce40e40fc789357ff5fa700ef79272ed3b0d141
hello-world-ttj <tijotjoseph003@gmail.com>
2023-12-28 09:58:37 +0530

    #merge conflict

--- src/ui/styledSearchField.jsx
+++ src/ui/styledSearchField.jsx
@@ -4,16 +4,11 @@ import { ReactComponent as SearchIcon } from '../assets/icons/Icon.svg'
 
 const SearchContainer = styled.div`
   position: relative;
-<<<<<<< HEAD
-  display: block;
-  width: 280px;
-=======
   width: 360px;
   height: 50px;
   padding-left : 20px;
   background-color:#2b2930;
   border-radius: 5px;
->>>>>>> 38c954064805c133794e08db15fa56c318d4c538
 `;
 
 const SearchInput = styled.input`
@@ -22,12 +17,7 @@ const SearchInput = styled.input`
   height: 100%;
   border: none;
   outline: none;
-<<<<<<< HEAD
-  width: 100%;
-  height: 50px;
-=======
   width: 90%;
->>>>>>> 38c954064805c133794e08db15fa56c318d4c538
   background-color:#2b2930;
   color:#CAC4D0;
 `;
