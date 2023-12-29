0df5f16bb777e230145e4a11cd22b5e2e36b7760
shimo91 <shimonaprakash@gmail.com>
2023-12-29 19:40:31 +0400

    added add and edit vid tag , implemented add vid tag to modal

--- src/components/tagManagement/Vid/AllVidCards.jsx
+++ src/components/tagManagement/Vid/AllVidCards.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import { Box, Stack, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import { Box, Modal, Stack, Typography } from '@mui/material'
 import StyledTable from '../../../ui/styledTable'
 import { DummyData } from '../../../assets/json/VidTableData'
 import StyledButton from '../../../ui/styledButton'
 import LastSynced from '../../../layout/LastSynced'
+import AddVidCards from './AddVidCards'
 
 const tableHeader = [
   'VID Tag',
@@ -15,15 +16,58 @@ const tableHeader = [
 ]
 
 export default function AllVidCards() {
+
+  const [open, setOpen] = useState(false);
+
+
+  // Function to open the modal
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  // Function to close the modal
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
       <LastSynced heading="VID Cards"/>
       <Box sx={{p:3}}>
         <Box display="flex" justifyContent="flex-end">
-            <StyledButton variant='secondary' width='150' mr='10'>Add</StyledButton>
+            <StyledButton variant='secondary' width='150' mr='10' onClick={handleOpen}>Add</StyledButton>
         </Box>
         <StyledTable headers={tableHeader} data={DummyData}/>
       </Box>
+      {/* Modal */}
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={modalStyle}>
+            <AddVidCards Close={handleClose} />
+        </Box>
+       
+      </Modal>
     </>
   )
 }
+
+
+
+// Modal style
+const modalStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 'auto', // Adjust width to fit your content or screen
+  boxShadow: 24,
+  borderRadius: 2,
+  color: '#fff', // White text for better visibility on dark background
+  outline: 'none', // Remove the focus ring
+  maxHeight: '100%',
+  overflowY:'auto'
+};
