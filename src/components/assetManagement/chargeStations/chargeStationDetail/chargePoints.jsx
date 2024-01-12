1f661fdc92f28b08473a28c1a3ab0341208eca1a
mhdyaseen8841 <riyaskh123@gmail.com>
2024-01-12 10:31:36 +0530

    asset management

--- src/components/assetManagement/chargeStations/chargeStationDetail/chargePoints.jsx
+++ src/components/assetManagement/chargeStations/chargeStationDetail/chargePoints.jsx
@@ -1,35 +1,48 @@
-import React,{useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { Box, Dialog, Stack, Typography } from '@mui/material'
 import { ReactComponent as ReloadIcon } from '../../../../assets/icons/reload.svg'
 import StyledButton from '../../../../ui/styledButton'
 import StyledTable from '../../../../ui/styledTable'
 import { chargePointsData } from '../../../../assets/json/chargepoints'
 import AddChargePoint from '../../chargePoints/AddChargePoint'
-export default function ChargePoints() {
-  const tableHeader = [
-    'CPID',
-    'OEM',
-    'Model',
-    'Tariff',
-    'Status',
-    'Published'
-  ]
+import { tableHeaderReplace } from '../../../../utils/tableHeaderReplace'
 
-  const [open,setOpen] = useState(false)
+const tableHeader = [
+  'CPID',
+  'OEM',
+  'Model',
+  'Tariff',
+  'Status',
+  'Published'
+]
+
+
+export default function ChargePoints({ data, ...props }) {
+
+
+  const [open, setOpen] = useState(false)
+
+  const [allChargePointsData, setAllChargePointsData] = useState([])
+  useEffect(() => {
+    setAllChargePointsData(tableHeaderReplace(data, ['CPID', 'name', 'type', 'charger_tariff', 'cpidStatus', 'cpidStatus', 'type'], tableHeader))
+  }, [data])
+
+
+
   return (
     <>
-          <Dialog
-      open={open}
-    >
-         <AddChargePoint/>
-    </Dialog>
+      <Dialog
+        open={open}
+      >
+        <AddChargePoint />
+      </Dialog>
       <Box
         sx={{
           display: 'flex',
           flexDirection: 'row',
           alignItems: 'center',
           backgroundColor: 'primary.grey',
-          p: {sx:1,md:2},
+          p: { sx: 1, md: 2 },
         }}>
         <Stack direction={'column'} sx={{ ml: 2 }}>
           <Typography variant='body1' sx={{ color: 'secondary.contrastText' }}>Charge-points</Typography>
@@ -40,12 +53,12 @@ export default function ChargePoints() {
           </Stack>
         </Stack>
         <Box sx={{ flexGrow: 1 }} />
-        <Box sx={{px:1}}>
-          <StyledButton variant='primary' style={{width:'100%',height:'45px',fontSize:'14px',fontWeight:'400'}} onClick={()=>setOpen(true)}> Add Chargepoint</StyledButton>
+        <Box sx={{ px: 1 }}>
+          <StyledButton variant='primary' style={{ width: '100%', height: '45px', fontSize: '14px', fontWeight: '400' }} onClick={() => setOpen(true)}> Add Chargepoint</StyledButton>
         </Box>
       </Box>
-      <Box sx={{p:3}}>
-        <StyledTable headers={tableHeader} data={chargePointsData} />
+      <Box sx={{ p: 3 }}>
+        <StyledTable headers={tableHeader} data={allChargePointsData} />
       </Box>
     </>
   )
