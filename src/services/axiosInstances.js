257422de5f3c4bb54b26228912c6afad26fefed2
XyvinTech <141544499+XyvinTech@users.noreply.github.com>
2024-01-16 17:25:33 +0530

    reference in form input

--- src/services/axiosInstances.js
+++ src/services/axiosInstances.js
@@ -5,8 +5,8 @@ let token = sessionStorage.getItem("userToken") || null;
 //! For Vehicle/Brand/transaction/Configuration/RFID/Notification services only
 
 export const LAMBDA_INSTANCES = axios.create({
-  // baseURL: 'https://dlupfxb3p6.execute-api.ap-south-1.amazonaws.com/api/v1',
-  baseURL: 'http://localhost:5689/api/v1',
+  baseURL: 'https://dlupfxb3p6.execute-api.ap-south-1.amazonaws.com/api/v1',
+  // baseURL: 'http://localhost:5689/api/v1',
   headers: {
     Authorization: "Bearer " + token,
     "Content-Type": "application/json",
@@ -27,8 +27,8 @@ export const CHARGING_STATION_INSTANCE = axios.create({
 
 //!for ev charge  machine, charge points, oem, ev models
 export const EV_MACHINE_INSTANCE = axios.create({
-  // baseURL: 'http://alb-762634556.ap-south-1.elb.amazonaws.com:5691/api/v1',
-  baseURL: 'http://localhost:5691/api/v1',
+  baseURL: 'http://alb-762634556.ap-south-1.elb.amazonaws.com:5691/api/v1',
+  // baseURL: 'http://localhost:5691/api/v1',
 
   headers: {
     Authorization: "Bearer " + token,
