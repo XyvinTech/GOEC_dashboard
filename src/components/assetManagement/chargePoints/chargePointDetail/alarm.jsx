fbca0111788faa98bec001c06404f644c747a357
riyaskh123 <riyaskh123@gmail.com>
2024-02-10 19:17:25 +0530

    bug fix

--- src/components/assetManagement/chargePoints/chargePointDetail/alarm.jsx
+++ src/components/assetManagement/chargePoints/chargePointDetail/alarm.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import { Box, Stack, Typography } from '@mui/material'
+import React, { useEffect } from 'react'
+import { Box, IconButton, Stack, Typography } from '@mui/material'
 import { ReactComponent as ReloadIcon } from '../../../../assets/icons/reload.svg'
 import StyledSearchField from '../../../../ui/styledSearchField'
 import { Tune, FileDownloadOutlined } from '@mui/icons-material'
-import StyledTable from '../../../../ui/styledTable'
+import StyledTable, { TableHeader } from '../../../../ui/styledTable'
 import { alarmData } from '../../../../assets/json/chargepoints'
+import { useState } from 'react'
+import { getAlarmsById } from '../../../../services/ocppAPI'
+import LastSynced from '../../../../layout/LastSynced'
+import { tableHeaderReplace } from '../../../../utils/tableHeaderReplace'
+import { searchAndFilter } from '../../../../utils/search'
 
-const StyledIconButton = ({icon, ...props}) => {
+const StyledIconButton = ({ icon, ...props }) => {
     return (
-        <Stack sx={{ backgroundColor: '#322F3B',px:2, justifyContent: 'center', alignItems: 'center', borderRadius: '4px' }} props>
+        <Stack sx={{ backgroundColor: '#322F3B', px: 2, justifyContent: 'center', alignItems: 'center', borderRadius: '4px' }} props>
             {icon && icon}
         </Stack>
     )
@@ -24,35 +29,29 @@ const tableHeader = [
 ]
 
 
-export default function Alarm() {
-    return (
-        <><Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
-                backgroundColor: 'primary.grey',
-                justifyContent: 'space-between',
-                p: 2
-            }}>
-            <Stack direction={'column'} sx={{ ml: 2 }}>
-                <Typography variant='body1' sx={{ color: 'secondary.contrastText' }}>Transactions</Typography>
-                <Stack direction={'row'} sx={{ alignItems: 'center' }} spacing={1}>
-                    <Typography sx={{ color: 'secondary.greytext', fontSize: 12 }}>Last synced</Typography>
-                    <Typography sx={{ color: 'success.main', fontSize: 12 }}>4 minutes ago</Typography>
-                    <ReloadIcon style={{ cursor: 'pointer' }} />
-                </Stack>
+export default function Alarm({ CPID }) {
+    const [filterValue, setFilterValue] = useState('')
 
-            </Stack>
-            <Stack direction={'row'} spacing={2}>
-                <StyledSearchField placeholder={'Search'} />
-                <StyledIconButton icon={<Tune sx={{ color: 'secondary.contrastText' }} />} />
-                <StyledIconButton icon={<FileDownloadOutlined sx={{ color: 'secondary.contrastText' }} />} />
-            </Stack>
-        </Box>
-        <Box sx={{p:3,overflow:'scroll'}}>
-        <StyledTable headers={tableHeader} data={alarmData} showActionCell={false}/>
-        </Box>
+    const [alarmList, setAlarmList] = useState([])
+    useEffect(() => {
+        getAlarmsById(CPID).then((res) => {
+            if (res.status) {
+                setAlarmList(tableHeaderReplace(res.result,['cpid','date','summary','connectorId','status','errorCode'],tableHeader) )
+            }
+        })
+    }, [])
+
+    return (
+        <>
+            <LastSynced heading="Charge Points">
+                <StyledSearchField placeholder={'Search'} onChange={(e) => {
+                    setFilterValue(e.target.value)
+                }} />
+                <IconButton sx={{ backgroundColor: 'secondary.button', borderRadius: '4px', px: 2 }}><Tune /></IconButton>
+            </LastSynced>
+            <Box sx={{ p: 3, overflow: 'scroll' }}>
+                <StyledTable headers={tableHeader} data={searchAndFilter(alarmList, filterValue)} showActionCell={false} />
+            </Box>
         </>
     )
 }
