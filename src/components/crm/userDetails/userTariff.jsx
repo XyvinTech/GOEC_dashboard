0ac033d0935c2a2f20246f66a702c6b1191d7725
hello-world-ttj <tijotjoseph003@gmail.com>
2024-02-12 12:29:03 +0530

    #reviews, favourites, tariff, vr details

--- src/components/crm/userDetails/userTariff.jsx
+++ src/components/crm/userDetails/userTariff.jsx
@@ -1,60 +1,77 @@
-import { Box, Grid } from '@mui/material';
-import React, { useState } from 'react'
-import LastSynced from '../../../layout/LastSynced';
-import TariffCard from './userTariff/tariffCard';
-import AssignTariff from './userTariff/assignTariff';
-
+import { Box, Grid } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import LastSynced from "../../../layout/LastSynced";
+import TariffCard from "./userTariff/tariffCard";
+import AssignTariff from "./userTariff/assignTariff";
+import { useLocation } from "react-router-dom";
+import { userchargingTariff } from "../../../services/userApi";
 
 const tariffData = [
   {
-    'tarrifName': 'Default',
-    'Location': 'Oberon Mall',
-    'CPID': 'GOEC1',
-    'Value': 15,
-    'Tax': 'GST Kerala',
-    'ServiceFee': '-'
+    tarrifName: "Default",
+    Location: "Oberon Mall",
+    CPID: "GOEC1",
+    Value: 15,
+    Tax: "GST Kerala",
+    ServiceFee: "-",
   },
   {
-    'tarrifName': 'Default',
-    'Location': 'Oberon Mall',
-    'CPID': 'GOEC1',
-    'Value': 15,
-    'Tax': 'GST Kerala',
-    'ServiceFee': '-'
+    tarrifName: "Default",
+    Location: "Oberon Mall",
+    CPID: "GOEC1",
+    Value: 15,
+    Tax: "GST Kerala",
+    ServiceFee: "-",
   },
   {
-    'tarrifName': 'Default',
-    'Location': 'Oberon Mall',
-    'CPID': 'GOEC1',
-    'Value': 15,
-    'Tax': 'GST Kerala',
-    'ServiceFee': '-'
+    tarrifName: "Default",
+    Location: "Oberon Mall",
+    CPID: "GOEC1",
+    Value: 15,
+    Tax: "GST Kerala",
+    ServiceFee: "-",
   },
   {
-    'tarrifName': 'Default',
-    'Location': 'Oberon Mall',
-    'CPID': 'GOEC1',
-    'Value': 15,
-    'Tax': 'GST Kerala',
-    'ServiceFee': '-'
-  }
-]
+    tarrifName: "Default",
+    Location: "Oberon Mall",
+    CPID: "GOEC1",
+    Value: 15,
+    Tax: "GST Kerala",
+    ServiceFee: "-",
+  },
+];
 
 export default function UserTariff() {
-  const [open,setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
+  const { state } = useLocation();
+  const [tariff, setTariff] = useState();
+
+  const getData = async () => {
+    const res = await userchargingTariff(state);
+    setTariff(res.result);
+  };
+
+  useEffect(() => {
+    getData();
+  }, [state]);
   return (
     <Box>
-      <AssignTariff open={open} onClose={()=>{setOpen(false)}}/>
-      <LastSynced heading={'Tarrif'} showButton={true} handleClick={()=>{setOpen(true)}}/>
-      <Grid container sx={{ p: { xs: 2, md: 4 } }} spacing={2}>
-        {
-          tariffData.map((item) => (
-            <Grid item xs={12} md={3} lg={3} xl={2}>
-              <TariffCard data={item} />
-            </Grid>
-          ))
-        }
+      <AssignTariff
+        open={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+      />
+      <LastSynced
+        heading={"Tarrif"}
+        showButton={true}
+        handleClick={() => {
+          setOpen(true);
+        }}
+      />
+      <Grid container sx={{ p: { xs: 2, md: 4 }, justifyContent:"center" }} spacing={2}>
+        <TariffCard data={tariff} />
       </Grid>
     </Box>
-  )
+  );
 }
--- src/services/reviewApi.js
+++ src/services/reviewApi.js
@@ -1,19 +1,28 @@
 import { REVIEW_INSTANCE } from "./axiosInstances";
 
 export async function getReviewBySation(id) {
-    try {
-      const response = await REVIEW_INSTANCE.get(`/review/${id}`);
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
+  try {
+    const response = await REVIEW_INSTANCE.get(`/review/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
   }
+}
 
-  export async function deleteReview(id) {
-    try {
-      const response = await REVIEW_INSTANCE.delete(`/review/${id}`);
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
+export async function deleteReview(id) {
+  try {
+    const response = await REVIEW_INSTANCE.delete(`/review/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
   }
+}
+
+export async function userReviews(Id) {
+  try {
+    const response = await REVIEW_INSTANCE.get(`review/filteredList?user=${Id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
