8c537bbd79fe7f88ffd829ecc48b0b23a353b0d6
XyvinTech <141544499+XyvinTech@users.noreply.github.com>
2024-02-13 19:21:22 +0530

    user rfid unassign

--- src/components/crm/userDetails/assignIDTags/RFID.jsx
+++ src/components/crm/userDetails/assignIDTags/RFID.jsx
@@ -1,31 +1,91 @@
-import { Box, Dialog } from '@mui/material';
-import React, { useState } from 'react'
-import LastSynced from '../../../../layout/LastSynced';
-import StyledTable from '../../../../ui/styledTable';
-import { RFIDData } from '../../../../assets/json/crm';
-import AssignRFID from './assignRFID';
-import { Transition } from '../../../../utils/DialogAnimation';
+import { Box, Dialog } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import LastSynced from "../../../../layout/LastSynced";
+import StyledTable from "../../../../ui/styledTable";
+import { RFIDData } from "../../../../assets/json/crm";
+import AssignRFID from "./assignRFID";
+import { Transition } from "../../../../utils/DialogAnimation";
+import { removeRfidTag, userRfidList } from "../../../../services/userApi";
+import { useParams } from "react-router-dom";
+import { tableHeaderReplace } from "../../../../utils/tableHeaderReplace";
+import { toast } from "react-toastify";
+
 
 const tableHeader = [
-    'RFID tag',
-    'Created on',
-    'Expires on',
-    'Validity',
-    'Status'
-]
+  "RFID tag",
+  "Created on",
+  "Expires on",
+  "Serial No",
+  "Status",
+];
 
 export default function RFID() {
 
-    const [open,setopen] = useState(false)
-    return (
-        <Box>
-            <Dialog fullWidth maxWidth={'md'} open={open} onClose={()=>{setopen(false)}} TransitionComponent={Transition}>
-                <AssignRFID />
-            </Dialog>
-            <LastSynced heading={'Assigned RFID'} showButton={true}  handleClick={()=>{setopen(true)}}/>
-            <Box sx={{ p: { xs: 2, md: 4 } }}>
-                <StyledTable headers={tableHeader} data={RFIDData} showActionCell={true} actions={['Unassign', 'view', 'Delete']} onActionClick={(e) => { console.log(e); }} />
-            </Box>
-        </Box>
-    )
+    const { id } = useParams();
+
+  const [data, setData] = useState([]);
+
+  const init = async () => {
+    try {
+      const res = await userRfidList(id)
+      setData(res.result)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const tableActionClick = async (e) => {
+    
+       let rfid = {"rfidTagId": e.data.id}
+       try {
+         await removeRfidTag(id,rfid)
+         init();
+        toast.success("Unassigned successfully!");
+
+       } catch (error) {
+        toast.error("Some error");
+        console.log(error);
+       }
+    
+    
+  }
+
+  const [open, setopen] = useState(false);
+  const rfidData = tableHeaderReplace(data, ['rfidTag', 'createdOn', 'expiry', 'serialNumber', 'status'], tableHeader)
+
+  return (
+    <Box>
+      <Dialog
+        fullWidth
+        maxWidth={"md"}
+        open={open}
+        onClose={() => {
+          setopen(false);
+        }}
+        TransitionComponent={Transition}
+      >
+        <AssignRFID />
+      </Dialog>
+      <LastSynced
+        heading={"Assigned RFID"}
+        showButton={true}
+        handleClick={() => {
+          setopen(true);
+        }}
+      />
+      <Box sx={{ p: { xs: 2, md: 4 } }}>
+        <StyledTable
+          headers={tableHeader}
+          data={rfidData}
+          showActionCell={true}
+          actions={["Unassign"]}
+          onActionClick={tableActionClick}
+        />
+      </Box>
+    </Box>
+  );
 }
